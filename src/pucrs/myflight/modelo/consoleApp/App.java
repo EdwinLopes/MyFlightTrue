210686f953535cf8060ccb547f300836412920f9
19111121 <edwin.almeida@edu.pucrs.br>
2019-09-11 18:33:23 -0300

    Teste

--- src/pucrs/myflight/modelo/consoleApp/App.java
+++ src/pucrs/myflight/modelo/consoleApp/App.java
@@ -3,6 +3,8 @@ package pucrs.myflight.modelo.consoleApp;
 public class App {
 
 	public static void main(String[] args) {
+
 		System.out.println("MyFlight project...");
+		System.out.println ("Teste commit");
 	}
 }
