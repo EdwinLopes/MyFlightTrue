3fec3621de8fe89074f4b39411f2765148a8695e
19107505 <aloisio.miguel@edu.pucrs.br>
2019-10-09 19:01:07 -0300

    emplementing the firs steps of the class 'GerenciadorRotas'

--- src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
 package pucrs.myflight.modelo;
 
 public class GerenciadorRotas {
+    private ArrayList<Rota> rotas;
+    
+    public GerenciadorRotas(){
+        rotas = new ArrayList<Rota>();
+    }
 
+    public void adicionar(Rota rota){
+        rotas.add(rota);
+    }
 
+    public ArrayList<Rota> listarTodas(){
+        return rotas;
+    }
+
+    public ArrayList<Rota> buscarPorOrigem(Aeroporto origem){
+        ArrayList<> retorno = new ArrayList<Rota>();
+        for (Rota r : rotas) {
+            if(r.getOrigem() == origem) retorno.add(origem);
+        }
+        return retorno;
+    }
+    
+    
 }
